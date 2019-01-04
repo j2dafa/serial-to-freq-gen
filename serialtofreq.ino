25c629b76f7f6c0cff6db91060a642ba6a49e3e4
j2dafa <19855911+j2dafa@users.noreply.github.com>
2019-01-04 18:54:52 +1100

    Update serialtofreq.ino
    Changed software serial to serial1

--- serialtofreq.ino
+++ serialtofreq.ino
@@ -1,40 +1,34 @@
 /*
   Software serial to frequency generator
 
-  This is based on Tom Igoe's software serial example, but with buffering of
-  sent data until the terminating carriage return (CR / 0xD / '\r'), so that
-  all the TX data goes out in a block with the characters close together.
+  Based on Martyn Davies code with modifications to use Serial1
+  instead of the Serial Software Library.
 
  This example code is in the public domain.
 
  */
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  Serial1.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-  // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
 }
 
 char buf[40];
 int count=0;
 
 void loop() { // run over and over
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
+  if (Serial1.available()) {
+    Serial.write(Serial1.read());
   }
   if (Serial.available()) {
     char ch = Serial.read();
     if(ch=='\r'){
       buf[count]=0;
-      mySerial.print(buf);
+      Serial1.print(buf);
       count = 0;   
     }else if(ch=='\n'){
       ;   
@@ -44,5 +38,3 @@ void loop() { // run over and over
   }
 
 }
-
-
